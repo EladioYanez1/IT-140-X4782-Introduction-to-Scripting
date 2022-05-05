afa39aef08f584224fc63272f5c4a0a089e5c940
Eladio Yanez <103149081+EladioYanez1@users.noreply.github.com>
2022-05-05 13:21:40 -0700

    Add files via upload

---
+++ Yanez_Eladio_Character_Counter.py
@@ -0,0 +1,9 @@
+phrase = input()
+#Rather than splitting the statement and creating complications, I thought it was easier to keep it as one string,
+#having 0 as the letter avoids for words being in place. 
+letter = phrase[0]
+#Search helps define where to look for the letter.
+search = phrase[1: ]
+#Having 0 represent the letter is really helpful for the search and avoiding complications. 
+amount = search.count(letter)
+print(amount)
